a206def90dab1cfb8ddda9863d341cafa4bc6b11
MikhedkoA <anrimih@ukr.net>
2020-09-01 18:43:14 +0300

    JS lesson1_17

---
+++ Homework/Javascript Lessons/Lesson17/assets/js/script.js
@@ -0,0 +1,17 @@
+/*Площадь треугольника*/
+let length_a = 7;
+let length_b = 3;
+let square_1 = length_a * length_b;
+console.log("Площадь треугольника:" + square_1);
+
+/*Площадь круга*/
+const pi = 3.14;
+let radius = 17;
+let square_2 = pi*radius*radius;
+console.log("Площадь круга:" + square_2);
+
+/*Возведение в степень*/
+let base = 2;
+let exp = 4;
+let v = base*base*base*base;
+console.log("Возведение числа " + base + " в степень " + exp + " равно " + v);
